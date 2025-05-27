41161f28d053fdec7d21d998169ddc1aa8e6049f
Yash Uppot <yashuppot@gmail.com>
2025-05-27 15:40:36 -0400

    Set up API endpoint structure

---
+++ backend/routes/tasks.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/Task');
+
+router.post('/', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+module.exports = router;
